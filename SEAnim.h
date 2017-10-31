73468f016b7a6fd5f6aaef5eed2c0a3d46b57194
dtzxporter <dtzxporter@users.noreply.github.com>
2017-10-30 23:11:06 -0400

    Update SEAnim.h
    Update SEAnim.h, to comply with specification changes

--- SEAnim.h
+++ SEAnim.h
@@ -33,7 +33,7 @@ extern "C"
 
 #define SEANIM_VERSION			1
 #define SEANIM_MAGIC			"SEAnim"
-#define SEANIM_SPEC_VERSION		"1.0.1"
+#define SEANIM_SPEC_VERSION		"1.1.0"
 #define SEANIM_LIB_VERSION		"1.0"
 
 	// Specifies how the data is interpreted by the importer
